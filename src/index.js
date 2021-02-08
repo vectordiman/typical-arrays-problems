e666fff02f7d86db97b62dbde2040f99b86da9e3
Diman <vector_diman@mail.ru>
2021-02-08 19:02:47 +0300

    add: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0)
+  {
+      let min = array[0];
+      for(let index = 1; index < array.length; index++)
+      {
+          if(min > array[index])
+              min = array[index];
+      }
+      return min;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length > 0)
+    {
+        let max = array[0];
+        for(let index = 1; index < array.length; index++)
+        {
+            if(max < array[index])
+                max = array[index];
+        }
+        return max;
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
+    if (array && array.length > 0)
+    {
+        let sum = 0;
+        for(let index = 0; index < array.length; index++)
+        {
+            sum += array[index]
+        }
+        return sum / array.length;
+    }
   return 0;
 }
